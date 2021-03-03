220347451beaafb7d9802514bd42f824dfbc3524
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-03-03 12:55:40 +0100

    Also remove unused import

--- src/test/java/no/unit/nva/cristin/projects/TestPairProvider.java
+++ src/test/java/no/unit/nva/cristin/projects/TestPairProvider.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
