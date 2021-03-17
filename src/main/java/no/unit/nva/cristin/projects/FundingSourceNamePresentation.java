3ba80a28bf8f8ca09e20c061747c6cf6dc0b2a0f
torarnet <6398417+torarnet@users.noreply.github.com>
2021-03-17 11:29:37 +0100

    NP-2157: Removed unused classes
    Updated coverage ratio in jacoco

--- src/main/java/no/unit/nva/cristin/projects/FundingSourceNamePresentation.java
+++
@@ -1,8 +0,0 @@
-package no.unit.nva.cristin.projects;
-
-public class FundingSourceNamePresentation {
-
-    public String name;
-    public String language;
-
-}
--- src/main/java/no/unit/nva/cristin/projects/TitlePresentation.java
+++
@@ -1,8 +0,0 @@
-package no.unit.nva.cristin.projects;
-
-public class TitlePresentation {
-
-    public String title;
-    public String language;
-
-}
