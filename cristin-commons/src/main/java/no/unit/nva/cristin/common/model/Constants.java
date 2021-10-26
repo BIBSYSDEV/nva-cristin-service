36c46a8227d5a09eb5b778dda23310f7307ee685
torarnet <6398417+torarnet@users.noreply.github.com>
2021-10-26 15:54:32 +0200

    NP-2749: Removed unused variable

--- cristin-commons/src/main/java/no/unit/nva/cristin/common/model/Constants.java
+++ cristin-commons/src/main/java/no/unit/nva/cristin/common/model/Constants.java
@@ -8,5 +8,4 @@ public class Constants {
     public static final String LINK = "link";
     public static final String REL_NEXT = "rel=\"next\"";
     public static final String REL_PREV = "rel=\"prev\"";
-    public static final String HTTPS = "https";
 }
