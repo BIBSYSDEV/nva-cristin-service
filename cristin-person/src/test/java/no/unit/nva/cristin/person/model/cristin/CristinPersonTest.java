2d7df1161439fb8e930b7dd4863a7bc9525179bc
torarnet <6398417+torarnet@users.noreply.github.com>
2021-11-04 11:41:19 +0100

    NP-3380: Check all data match against node in CristinPerson object

--- cristin-person/src/test/java/no/unit/nva/cristin/person/model/cristin/CristinPersonTest.java
+++ cristin-person/src/test/java/no/unit/nva/cristin/person/model/cristin/CristinPersonTest.java
@@ -1,6 +1,7 @@
 package no.unit.nva.cristin.person.model.cristin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -18,7 +19,28 @@ public class CristinPersonTest {
     void cristinModelBuildsCorrectlyWhenDeserializingPersonJson() throws IOException {
         String body = getBodyFromResource(CRISTIN_GET_PERSON_JSON);
         CristinPerson person = fromJson(body, CristinPerson.class);
-        assertEquals("359084", person.getCristinPersonId());
+        JsonNode node = OBJECT_MAPPER.readTree(body);
+
+        assertEquals(person.getCristinPersonId(), node.get("cristin_person_id").asText());
+        assertEquals(person.getOrcid().getId(), node.get("orcid").get("id").asText());
+        assertEquals(person.getFirstName(), node.get("first_name").asText());
+        assertEquals(person.getSurname(), node.get("surname").asText());
+        assertEquals(person.getFirstNamePreferred(), node.get("first_name_preferred").asText());
+        assertEquals(person.getSurnamePreferred(), node.get("surname_preferred").asText());
+        assertEquals(person.getTel(), node.get("tel").asText());
+        assertEquals(person.getPictureUrl(), node.get("picture_url").asText());
+        assertEquals(person.getAffiliations().get(0).getActive(),
+            node.get("affiliations").get(0).get("active").asBoolean());
+        assertEquals(person.getAffiliations().get(0).getUnit().getCristinUnitId(),
+            node.get("affiliations").get(0).get("unit").get("cristin_unit_id").asText());
+        assertEquals(person.getAffiliations().get(0).getUnit().getUrl(),
+            node.get("affiliations").get(0).get("unit").get("url").asText());
+        assertEquals(person.getAffiliations().get(1).getActive(),
+            node.get("affiliations").get(1).get("active").asBoolean());
+        assertEquals(person.getAffiliations().get(1).getUnit().getCristinUnitId(),
+            node.get("affiliations").get(1).get("unit").get("cristin_unit_id").asText());
+        assertEquals(person.getAffiliations().get(1).getUnit().getUrl(),
+            node.get("affiliations").get(1).get("unit").get("url").asText());
     }
 
     @Test
