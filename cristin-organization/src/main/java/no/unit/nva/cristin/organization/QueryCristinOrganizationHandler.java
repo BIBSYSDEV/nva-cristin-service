8a9119cc5910b7e903f4082866cffb0a1e683975
torarnet <6398417+torarnet@users.noreply.github.com>
2021-11-23 12:30:23 +0100

    NP-3445: QueryCristinOrganizationHandler now extends CristinQueryHandler
    Removed methods that can be called from superclass instead

--- cristin-organization/src/main/java/no/unit/nva/cristin/organization/QueryCristinOrganizationHandler.java
+++ cristin-organization/src/main/java/no/unit/nva/cristin/organization/QueryCristinOrganizationHandler.java
@@ -2,14 +2,13 @@ package no.unit.nva.cristin.organization;
 
 
 import com.amazonaws.services.lambda.runtime.Context;
-import no.unit.nva.cristin.common.Utils;
+import no.unit.nva.cristin.common.handler.CristinQueryHandler;
 import no.unit.nva.cristin.model.SearchResponse;
 import no.unit.nva.model.Organization;
-import no.unit.nva.utils.UriUtils;
-import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.BadRequestException;
+import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 
 import java.net.HttpURLConnection;
@@ -18,28 +17,17 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static no.unit.nva.cristin.model.Constants.DEFAULT_NUMBER_OF_RESULTS;
-import static no.unit.nva.cristin.model.Constants.FIRST_PAGE;
 import static no.unit.nva.cristin.model.JsonPropertyNames.LANGUAGE;
 import static no.unit.nva.cristin.model.JsonPropertyNames.NUMBER_OF_RESULTS;
 import static no.unit.nva.cristin.model.JsonPropertyNames.PAGE;
 import static no.unit.nva.cristin.model.JsonPropertyNames.QUERY;
 import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_INVALID_QUERY_PARAMS_ON_LOOKUP;
 import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_LANGUAGE_INVALID;
-import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_NUMBER_OF_RESULTS_VALUE_INVALID;
-import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_PAGE_VALUE_INVALID;
-import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_QUERY_MISSING_OR_HAS_ILLEGAL_CHARACTERS;
-import static nva.commons.core.attempt.Try.attempt;
 
 @SuppressWarnings("unused")
-public class QueryCristinOrganizationHandler extends ApiGatewayHandler<Void, SearchResponse<Organization>> {
+public class QueryCristinOrganizationHandler extends CristinQueryHandler<Void, SearchResponse<Organization>> {
 
-    protected static final String DEFAULT_LANGUAGE_CODE = "nb";
-    private static final char CHARACTER_DASH = '-';
-    private static final char CHARACTER_COMMA = ',';
-    private static final char CHARACTER_PERIOD = '.';
     private static final Set<String> VALID_QUERY_PARAMS = Set.of(QUERY, LANGUAGE, PAGE, NUMBER_OF_RESULTS);
-    private static final Set<String> VALID_LANGUAGE_CODES = Set.of("en", "nb", "nn");
     private final transient CristinApiClient cristinApiClient;
 
     @JacocoGenerated
@@ -48,26 +36,15 @@ public class QueryCristinOrganizationHandler extends ApiGatewayHandler<Void, Sea
     }
 
     public QueryCristinOrganizationHandler(CristinApiClient cristinApiClient) {
-        super(Void.class);
+        super(Void.class, new Environment());
         this.cristinApiClient = cristinApiClient;
-    }
-
-    protected static Optional<String> getQueryParam(RequestInfo requestInfo, String queryParameter) {
-        return attempt(() -> requestInfo.getQueryParameter(queryParameter)).toOptional();
-    }
-
-    protected static String getValidLanguage(RequestInfo requestInfo) throws BadRequestException {
-        return Optional.of(getQueryParam(requestInfo, LANGUAGE)
-                        .orElse(DEFAULT_LANGUAGE_CODE))
-                .filter(VALID_LANGUAGE_CODES::contains)
-                .orElseThrow(() -> new BadRequestException(ERROR_MESSAGE_LANGUAGE_INVALID));
     }
 
     @Override
     protected SearchResponse<Organization> processInput(Void input, RequestInfo requestInfo, Context context)
             throws ApiGatewayException {
 
-        validateThatSuppliedQueryParamsIsSupported(requestInfo);
+        validateQueryParamKeys(requestInfo);
 
         Map<String, String> requestQueryParams = new ConcurrentHashMap<>();
         requestQueryParams.put(LANGUAGE, getValidLanguage(requestInfo));
@@ -87,7 +64,8 @@ public class QueryCristinOrganizationHandler extends ApiGatewayHandler<Void, Sea
         return HttpURLConnection.HTTP_OK;
     }
 
-    private void validateThatSuppliedQueryParamsIsSupported(RequestInfo requestInfo) throws BadRequestException {
+    @Override
+    protected void validateQueryParamKeys(RequestInfo requestInfo) throws BadRequestException {
         if (!VALID_QUERY_PARAMS.containsAll(requestInfo.getQueryParameters().keySet())) {
             throw new BadRequestException(ERROR_MESSAGE_INVALID_QUERY_PARAMS_ON_LOOKUP);
         }
@@ -98,45 +76,6 @@ public class QueryCristinOrganizationHandler extends ApiGatewayHandler<Void, Sea
 
         return Optional.of(cristinApiClient.queryInstitutions(requestQueryParams))
                 .orElseThrow(() -> new BadRequestException(ERROR_MESSAGE_LANGUAGE_INVALID));
-    }
-
-    private String getValidQuery(RequestInfo requestInfo) throws BadRequestException {
-        return getQueryParam(requestInfo, QUERY)
-                .filter(this::isValidQuery)
-                .map(UriUtils::escapeWhiteSpace)
-                .orElseThrow(() -> new BadRequestException(ERROR_MESSAGE_QUERY_MISSING_OR_HAS_ILLEGAL_CHARACTERS));
-    }
-
-    private String getValidPage(RequestInfo requestInfo) throws BadRequestException {
-        return Optional.of(getQueryParam(requestInfo, PAGE)
-                        .orElse(FIRST_PAGE))
-                .filter(Utils::isPositiveInteger)
-                .orElseThrow(() -> new BadRequestException(ERROR_MESSAGE_PAGE_VALUE_INVALID));
-    }
-
-    private String getValidNumberOfResults(RequestInfo requestInfo) throws BadRequestException {
-        return Optional.of(getQueryParam(requestInfo, NUMBER_OF_RESULTS)
-                        .orElse(DEFAULT_NUMBER_OF_RESULTS))
-                .filter(Utils::isPositiveInteger)
-                .orElseThrow(() -> new BadRequestException(ERROR_MESSAGE_NUMBER_OF_RESULTS_VALUE_INVALID));
-    }
-
-    private boolean isValidQuery(String str) {
-        char[] charArray = str.toCharArray();
-        for (char c : charArray) {
-            if (!isValidCharacter(c)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isValidCharacter(char c) {
-        return Character.isWhitespace(c)
-                || Character.isLetterOrDigit(c)
-                || c == CHARACTER_DASH
-                || c == CHARACTER_COMMA
-                || c == CHARACTER_PERIOD;
     }
 
 }
