9ca97eb600aff1b3558924d891aaf4c5ce55c841
svenngjeto <svenn.gjeto@unit.no>
2021-12-05 18:22:04 +0100

    Fixed test type, jUnit not Karate in testrunner

--- cristin-project/src/test/java/no/unit/nva/cristin/projects/ProjectKarateTestRunner.java
+++ cristin-project/src/test/java/no/unit/nva/cristin/projects/ProjectKarateTestRunner.java
@@ -2,11 +2,12 @@ package no.unit.nva.cristin.projects;
 
 import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 @Tag("karateTest")
 class ProjectKarateTestRunner {
 
-    @Karate.Test
+    @Test
     Karate testProject() {
         return Karate.run("project").relativeTo(getClass()).outputCucumberJson(true);
 
