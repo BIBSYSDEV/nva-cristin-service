22a596c64cefa8a2ac06b10495e342e186c7546d
svenngjeto <svenn.gjeto@unit.no>
2021-12-08 13:45:31 +0100

    exclude doesn't work but runs karatetests

--- cristin-organization/src/test/java/no/unit/nva/cristin/organization/OrganizationKarateTestRunner.java
+++ cristin-organization/src/test/java/no/unit/nva/cristin/organization/OrganizationKarateTestRunner.java
@@ -4,15 +4,13 @@ import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @Tag("karateTest")
 public class OrganizationKarateTestRunner {
 
-    @Test
-    void runOrganizationKarateTest() {
-        Karate.run("organization").relativeTo(getClass()).outputCucumberJson(true);
+    @Karate.Test
+    @Tag("karateTest")
+    Karate runOrganizationKarateTest() {
+        return Karate.run().relativeTo(getClass()).outputCucumberJson(true);
     }
-
 
 }
