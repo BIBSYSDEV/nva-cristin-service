af746649700112e120b08f88e73668aa5eaf6da7
svenngjeto <svenn.gjeto@unit.no>
2022-01-18 12:50:58 +0100

    Code refactor

--- cristin-commons/src/main/java/no/unit/nva/utils/AccessUtils.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/AccessUtils.java
@@ -7,13 +7,18 @@ public class AccessUtils {
 
     public static final String EDIT_OWN_INSTITUTION_USERS = "EDIT_OWN_INSTITUTION_USERS";
 
+    /**
+     * Validate Requesters access to a resource.
+     * @param requestInfo information from request
+     * @throws ForbiddenException thrown when user has no access to requested resource
+     */
     public static  void validateAccess(RequestInfo requestInfo) throws ForbiddenException {
-        if (!requesterHasAccessToReadNationalIdentificationNumber(requestInfo)) {
+        if (!requesterHasAccessRightToUseNationalIdentificationNumber(requestInfo)) {
             throw new ForbiddenException();
         }
     }
 
-    private static boolean requesterHasAccessToReadNationalIdentificationNumber(RequestInfo requestInfo) {
+    private static boolean requesterHasAccessRightToUseNationalIdentificationNumber(RequestInfo requestInfo) {
         return requestInfo.getAccessRights().contains(EDIT_OWN_INSTITUTION_USERS);
     }
 
