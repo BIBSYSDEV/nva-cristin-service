0c53b1b5089bf53f3479a80df5f8502a1a51d756
torarnet <6398417+torarnet@users.noreply.github.com>
2022-01-19 10:40:13 +0100

    NP-3542: New fields

--- cristin-person/src/main/java/no/unit/nva/cristin/person/model/nva/PersonInstitutionInfo.java
+++ cristin-person/src/main/java/no/unit/nva/cristin/person/model/nva/PersonInstitutionInfo.java
@@ -1,27 +1,49 @@
 package no.unit.nva.cristin.person.model.nva;
 
+import static no.unit.nva.cristin.model.JsonPropertyNames.CONTEXT;
+import static no.unit.nva.cristin.model.JsonPropertyNames.EMAIL;
+import static no.unit.nva.cristin.model.JsonPropertyNames.ID;
+import static no.unit.nva.cristin.model.JsonPropertyNames.PHONE;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.net.URI;
 import java.util.Objects;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.JsonSerializable;
 
 @JacocoGenerated
+@JsonPropertyOrder({CONTEXT, ID, EMAIL, PHONE})
 public class PersonInstitutionInfo implements JsonSerializable {
 
+    @JsonProperty(CONTEXT)
+    private static final String context = "https://ontology.org";
+    private final URI id;
     private final String email;
     private final String phone;
 
     /**
      * Creates a PersonInstitutionInfo for serialization to client.
      *
+     * @param id    Identifier for this resource.
      * @param email Person email at this institution.
      * @param phone Person phone at this institution.
      */
     @JsonCreator
-    public PersonInstitutionInfo(@JsonProperty("email") String email, @JsonProperty("phone") String phone) {
+    public PersonInstitutionInfo(@JsonProperty("id") URI id, @JsonProperty("email") String email,
+                                 @JsonProperty("phone") String phone) {
+        this.id = id;
         this.email = email;
         this.phone = phone;
+    }
+
+    @JsonProperty(CONTEXT)
+    public String getContext() {
+        return context;
+    }
+
+    public URI getId() {
+        return id;
     }
 
     public String getEmail() {
@@ -41,7 +63,9 @@ public class PersonInstitutionInfo implements JsonSerializable {
             return false;
         }
         PersonInstitutionInfo that = (PersonInstitutionInfo) o;
-        return Objects.equals(getEmail(), that.getEmail())
+        return Objects.equals(getContext(), that.getContext())
+            && Objects.equals(getId(), that.getId())
+            && Objects.equals(getEmail(), that.getEmail())
             && Objects.equals(getPhone(), that.getPhone());
     }
 
@@ -52,6 +76,6 @@ public class PersonInstitutionInfo implements JsonSerializable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(getEmail(), getPhone());
+        return Objects.hash(getContext(), getId(), getEmail(), getPhone());
     }
 }
