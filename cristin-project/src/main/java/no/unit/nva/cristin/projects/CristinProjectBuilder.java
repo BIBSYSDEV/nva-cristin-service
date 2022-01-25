db1a8352899c1de56e64f5ca7f3ebe36fa450423
svenngjeto <svenn.gjeto@unit.no>
2022-01-25 16:48:18 +0100

    Creating CristinProject from NVA Project

--- cristin-project/src/main/java/no/unit/nva/cristin/projects/CristinProjectBuilder.java
+++ cristin-project/src/main/java/no/unit/nva/cristin/projects/CristinProjectBuilder.java
@@ -1,49 +1,123 @@
 package no.unit.nva.cristin.projects;
 
+import no.unit.nva.cristin.model.CristinInstitution;
+import no.unit.nva.cristin.projects.model.cristin.CristinFundingSource;
+import no.unit.nva.cristin.projects.model.cristin.CristinPerson;
 import no.unit.nva.cristin.projects.model.cristin.CristinProject;
+import no.unit.nva.cristin.projects.model.cristin.CristinRole;
+import no.unit.nva.cristin.projects.model.nva.Funding;
+import no.unit.nva.cristin.projects.model.nva.NvaContributor;
 import no.unit.nva.cristin.projects.model.nva.NvaProject;
+import no.unit.nva.model.Organization;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CristinProjectBuilder {
 
+    private static final Map<String, String> NvaTypesToCristinRoles = Map.of(
+            "ProjectManager",
+            "PRO_MANAGER",
+            "ProjectParticipant",
+            "PRO_PARTICIPANT");
     private final transient CristinProject cristinProject;
     private final transient NvaProject nvaProject;
-
 
     public CristinProjectBuilder(NvaProject nvaProject) {
         this.nvaProject = nvaProject;
         this.cristinProject = new CristinProject();
     }
 
+    public static String extractLastPathElement(URI id) {
+        return id.getPath().substring(id.getPath().lastIndexOf('/') + 1);
+    }
+
+    /**
+     * Build an CristinProject representation from given NvaProject.
+     *
+     * @return valid CristinProject containing data from source NvaProject
+     */
     public CristinProject build() {
 
         cristinProject.setCristinProjectId(extractLastPathElement(nvaProject.getId()));
         cristinProject.setMainLanguage(extractLastPathElement(nvaProject.getLanguage()));
-        Map<String, String> title = new ConcurrentHashMap<>();
-        title.put(extractLastPathElement(nvaProject.getLanguage()), nvaProject.getTitle());
-        cristinProject.setTitle(title);
+        cristinProject.setTitle(extractTitles(nvaProject));
         cristinProject.setStatus(nvaProject.getStatus().name());
-        //         String type;
-        //         List<Map<String, String>> identifiers;
-        //         String title;
-        //         URI language;
-        //         List<Map<String, String>> alternativeTitles;
-        //         Instant startDate;
-        //         Instant endDate;
-        //         List<Funding> funding;
-        //         Organization coordinatingInstitution;
-        //         List<NvaContributor> contributors;
-        //         ProjectStatus status;
-        //         Map<String, String>  academicSummary;
-        //         Map<String, String>  popularScientificSummary;
+        cristinProject.setStartDate(nvaProject.getStartDate());
+        cristinProject.setEndDate(nvaProject.getEndDate());
+        cristinProject.setAcademicSummary(extractSummary(nvaProject.getAcademicSummary()));
+        cristinProject.setPopularScientificSummary(extractSummary(nvaProject.getPopularScientificSummary()));
+        cristinProject.setProjectFundingSources(extractFundings(nvaProject.getFunding()));
+        cristinProject.setParticipants(extractContributors(nvaProject.getContributors()));
+        cristinProject.setCoordinatingInstitution(
+                new CristinOrganizationBuilder(nvaProject.getCoordinatingInstitution()).build());
 
         return cristinProject;
     }
 
-    private String extractLastPathElement(URI id) {
-        return id.getPath().substring(id.getPath().lastIndexOf('/') + 1);
+    private List<CristinPerson> extractContributors(List<NvaContributor> contributors) {
+        List<CristinPerson> participants = new ArrayList<>();
+        for (NvaContributor contributor : contributors) {
+            CristinPerson cristinPerson = getCristinPerson(contributor);
+            participants.add(cristinPerson);
+        }
+        return participants;
+    }
+
+    private CristinPerson getCristinPerson(NvaContributor contributor) {
+        CristinPerson cristinPerson = new CristinPerson();
+        cristinPerson.setCristinPersonId(extractLastPathElement(contributor.getIdentity().getId()));
+        cristinPerson.setUrl(contributor.getIdentity().getId().toString());
+        cristinPerson.setFirstName(contributor.getIdentity().getFirstName());
+        cristinPerson.setSurname(contributor.getIdentity().getLastName());
+        cristinPerson.setRoles(extractCristinRoles(contributor));
+        return cristinPerson;
+    }
+
+    private List<CristinRole> extractCristinRoles(NvaContributor contributor) {
+        CristinRole cristinRole = new CristinRole();
+        cristinRole.setRoleCode(NvaTypesToCristinRoles.get(contributor.getType()));
+        cristinRole.setInstitution(toCristinInstitution(contributor.getAffiliation()));
+        return List.of(cristinRole);
+    }
+
+    private CristinInstitution toCristinInstitution(Organization affiliation) {
+        CristinInstitution institution = new CristinInstitution();
+        institution.setInstitutionName(affiliation.getName());
+        institution.setUrl(affiliation.getId().toString());
+        institution.setCristinInstitutionId(extractLastPathElement(affiliation.getId()));
+        return institution;
+    }
+
+    private List<CristinFundingSource> extractFundings(List<Funding> fundings) {
+        List<CristinFundingSource> cristinFundings = new ArrayList();
+        for (Funding funding : fundings) {
+            CristinFundingSource cristinFundingSource = getCristinFundingSource(funding);
+            cristinFundings.add(cristinFundingSource);
+        }
+        return cristinFundings;
+    }
+
+    private CristinFundingSource getCristinFundingSource(Funding funding) {
+        CristinFundingSource cristinFundingSource = new CristinFundingSource();
+        cristinFundingSource.setFundingSourceCode(funding.getSource().getCode());
+        cristinFundingSource.setFundingSourceName(funding.getSource().getNames());
+        cristinFundingSource.setProjectCode(funding.getCode());
+        return cristinFundingSource;
+    }
+
+    private Map<String, String> extractSummary(Map<String, String> summary) {
+        return Collections.unmodifiableMap(summary);
+    }
+
+    private Map<String, String> extractTitles(NvaProject nvaProject) {
+        Map<String, String> titles = new ConcurrentHashMap<>();
+        titles.put(extractLastPathElement(nvaProject.getLanguage()), nvaProject.getTitle());
+        nvaProject.getAlternativeTitles().stream().forEach(titles::putAll);
+        return titles;
     }
 }
--- cristin-project/src/test/java/no/unit/nva/cristin/projects/CristinProjectBuilderTest.java
+++ cristin-project/src/test/java/no/unit/nva/cristin/projects/CristinProjectBuilderTest.java
@@ -1,18 +1,33 @@
 package no.unit.nva.cristin.projects;
 
 import no.unit.nva.cristin.projects.model.cristin.CristinProject;
+import no.unit.nva.cristin.projects.model.nva.Funding;
+import no.unit.nva.cristin.projects.model.nva.FundingSource;
+import no.unit.nva.cristin.projects.model.nva.NvaContributor;
 import no.unit.nva.cristin.projects.model.nva.NvaProject;
+import no.unit.nva.cristin.projects.model.nva.Person;
+import no.unit.nva.model.Organization;
 import no.unit.nva.utils.UriUtils;
 import nva.commons.core.language.LanguageMapper;
+import nva.commons.core.paths.UriWrapper;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static no.unit.nva.cristin.model.Constants.CRISTIN_API_URL;
+import static no.unit.nva.cristin.model.Constants.INSTITUTION_PATH;
+import static no.unit.nva.cristin.model.Constants.PERSON_PATH;
 import static no.unit.nva.cristin.projects.NvaProjectBuilder.CRISTIN_IDENTIFIER_TYPE;
 import static no.unit.nva.cristin.projects.NvaProjectBuilder.PROJECT_TYPE;
 import static no.unit.nva.cristin.projects.NvaProjectBuilder.TYPE;
 import static no.unit.nva.cristin.projects.NvaProjectBuilder.VALUE;
+import static no.unit.nva.testutils.RandomDataGenerator.randomInstant;
 import static no.unit.nva.testutils.RandomDataGenerator.randomInteger;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,10 +36,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class CristinProjectBuilderTest {
 
     private static final String NORWEGIAN = "no";
+    private static final String[] LANGUAGES = {"en", "nb", "nn"};
+    private static final String[] CONTRIBUTOR_TYPES = {"ProjectManager", "ProjectParticipant"};
+    private static final String[] ROLE_TYPES = {"PRO_MANAGER", "PRO_PARTICIPANT"};
+
 
     @Test
     void shouldRemainValidProjectWhenDoubleConverted() {
-        NvaProject expected = createMinimalValidProject();
+        final NvaProject expected = createRandomNvaProject();
         final CristinProject cristinProject = expected.toCristinProject();
         assertNotNull(cristinProject);
         NvaProject actual = cristinProject.toNvaProject();
@@ -32,7 +51,7 @@ class CristinProjectBuilderTest {
         assertEquals(expected, actual);
     }
 
-    private NvaProject createMinimalValidProject() {
+    private NvaProject createRandomNvaProject() {
         NvaProject nvaProject = new NvaProject();
         nvaProject.setType(PROJECT_TYPE);
         final String identifier = randomInteger().toString();
@@ -41,8 +60,81 @@ class CristinProjectBuilderTest {
         nvaProject.setTitle(randomString());
         nvaProject.setLanguage(LanguageMapper.toUri(NORWEGIAN));
         nvaProject.setStatus(ProjectStatus.NOTSTARTED);
+        nvaProject.setAlternativeTitles(randomListOfTitles());
+        nvaProject.setStartDate(randomInstant());
+        nvaProject.setEndDate(randomInstant());
+        nvaProject.setAcademicSummary(randomSummary());
+        nvaProject.setPopularScientificSummary(randomSummary());
+        nvaProject.setFunding(randomFunding());
+        nvaProject.setContributors(randomContributors());
+        nvaProject.setCoordinatingInstitution(randomOrganization());
 
         return nvaProject;
+    }
+
+    private URI semiRandomPersonId(String identifier) {
+        return new UriWrapper(CRISTIN_API_URL).addChild(PERSON_PATH).addChild(identifier).getUri();
+    }
+
+
+    private URI semiRandomOrganizationId(String identifier) {
+        return new UriWrapper(CRISTIN_API_URL).addChild(INSTITUTION_PATH).addChild(identifier).getUri();
+    }
+
+    private List<NvaContributor> randomContributors() {
+        List<NvaContributor> contributors = new ArrayList<>();
+        for (int i = 0; i <= randomInteger(5); i++) {
+            NvaContributor contributor = new NvaContributor();
+            contributor.setAffiliation(randomOrganization());
+            contributor.setIdentity(randomPerson());
+            contributor.setType(randomContributorType());
+            contributors.add(contributor);
+        }
+        return contributors;
+    }
+
+    private String randomContributorType() {
+        return CONTRIBUTOR_TYPES[randomInteger(CONTRIBUTOR_TYPES.length)];
+    }
+
+    private Person randomPerson() {
+        return new Person(semiRandomPersonId(randomString()), randomString(), randomString());
+    }
+
+    private List<Funding> randomFunding() {
+        List<Funding> fundings = new ArrayList<>();
+        for (int i = 0; i < randomInteger(7); i++) {
+            String fundingSourceCode = randomString();
+            final FundingSource fundingSource = new FundingSource(randomNamesMap(), fundingSourceCode);
+            final String fundingCode = randomString();
+            Funding funding = new Funding(fundingSource, fundingCode);
+            fundings.add(funding);
+        }
+        return fundings;
+    }
+
+    private Map<String, String> randomNamesMap() {
+        return Map.of(randomLanguageCode(), randomString());
+    }
+
+    private Map<String, String> randomSummary() {
+        return Collections.unmodifiableMap(Arrays.stream(LANGUAGES)
+                .collect(Collectors.toMap(languageCode -> languageCode, languageCode -> randomString())));
+    }
+
+    private Organization randomOrganization() {
+        return new Organization.Builder()
+                .withId(semiRandomOrganizationId(randomString()))
+                .withName(randomNamesMap())
+                .build();
+    }
+
+    private List<Map<String, String>> randomListOfTitles() {
+        return List.of(Map.of(randomLanguageCode(), randomString()));
+    }
+
+    private String randomLanguageCode() {
+        return LANGUAGES[randomInteger(LANGUAGES.length)];
     }
 
 
