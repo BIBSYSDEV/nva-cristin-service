9434c91623392260e8095c156b4d400569ab22fa
svenngjeto <svenn.gjeto@unit.no>
2022-03-09 14:22:43 +0100

    update regex

--- cristin-commons/src/main/java/no/unit/nva/utils/SqlRecognizer.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/SqlRecognizer.java
@@ -4,32 +4,37 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SqlRecognizer {
 
-    private static final Set<String> sqlStatements = Set.of("'",
-            "and",
-            "exec",
-            "insert",
-            "select",
-            "delete",
-            "update",
-            "create",
-            "alter",
-            "count",
-            "drop",
-            "chr",
-            "mid",
-            "master",
-            "truncate",
-            "char",
-            "declare",
-            ";",
-            "or",
-            "-",
-            ",");
+    private static final Set<String> suspicousStatements = Set.of("''", "--", ";","$","=");
+    private static Pattern pattern = Pattern.compile("[\\p{Alnum}+\\s+-/–'&:();'#”«»´’_\"“`„‘’!^‐]+",Pattern.UNICODE_CHARACTER_CLASS);
 
+
+//    private static final Set<String> sqlStatements = Set.of("'",
+//            "and",
+//            "exec",
+//            "insert",
+//            "select",
+//            "delete",
+//            "update",
+//            "create",
+//            "alter",
+//            "count",
+//            "drop",
+//            "chr",
+//            "mid",
+//            "master",
+//            "truncate",
+//            "char",
+//            "declare",
+//            ";",
+//            "or",
+//            "-",
+//            ",");
+//
     /**
      * Checks if there are possible SQL statements in a string.
      * @param str String to checked for SQL statements
@@ -37,7 +42,19 @@ public class SqlRecognizer {
      */
     public static boolean containsSqlInjectionFragmentCandidate(String str) {
         Set<String> queryFragments  = new HashSet(List.of(str.toLowerCase(Locale.getDefault()).split(" ")));
-        Set intersect = queryFragments.stream().filter(sqlStatements::contains).collect(Collectors.toSet());
+        Set intersect = queryFragments.stream().filter(suspicousStatements::contains).collect(Collectors.toSet());
         return !intersect.isEmpty();
+    }
+
+    public static boolean containsSuspicousInjectionFragmentCandidate(String str) {
+        Set<String> queryFragments  = new HashSet(List.of(str.toLowerCase(Locale.getDefault()).split("\\s")));
+        Set intersect = queryFragments.stream().filter(suspicousStatements::contains).collect(Collectors.toSet());
+
+        // |\\w|\\s+|'\\-'|'\\,','\\/','\\.')+"
+//        Pattern pattern = Pattern.compile("([p{Alpha}]+|\\w+)+");
+        final boolean onlyLegalCharacters = pattern.matcher(str).matches();
+        final boolean empty = intersect.isEmpty();
+        final boolean result = !empty || !onlyLegalCharacters;
+        return result;
     }
 }
