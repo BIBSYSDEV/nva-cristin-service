edebfe73f2cb0a6d64977f699584fa0699d6db6a
svenngjeto <svenn.gjeto@unit.no>
2022-03-11 11:50:13 +0100

    Using auth0 jwt token utils

--- cristin-commons/src/main/java/no/unit/nva/utils/AccessUtils.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/AccessUtils.java
@@ -1,16 +1,16 @@
 package no.unit.nva.utils;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.JsonNode;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ForbiddenException;
-import nva.commons.core.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.StringReader;
-import java.util.Base64;
+import java.util.Date;
 import java.util.Optional;
 
+import static com.auth0.jwt.JWT.decode;
 import static java.util.Objects.nonNull;
 import static no.unit.nva.cristin.common.client.ApiClient.AUTHORIZATION;
 
@@ -23,12 +23,12 @@ public class AccessUtils {
     public static final String ACCESS_TOKEN_CLAIMS_FIELD = "claims";
     public static final String AUTHORIZER_FIELD = "authorizer";
     public static final String USER_DOES_NOT_HAVE_REQUIRED_ACCESS_RIGHT =
-        "User:{} does not have required access right:{}";
-    private static final String BACKEND_SCOPE_AS_DEFINED_IN_IDENTITY_SERVICE = "https://api.nva.unit.no/scopes/backend";
+            "User:{} does not have required access right:{}";
     public static final String BEARER = "Bearer";
     public static final String DOT_SEPARATOR = "\\.";
     public static final int PAYLOAD_SEGMENT = 1;
     public static final String CUSTOM_ACCESS_RIGHTS = "custom:accessRights";
+    private static final String BACKEND_SCOPE_AS_DEFINED_IN_IDENTITY_SERVICE = "https://api.nva.unit.no/scopes/backend";
     private static final Logger logger = LoggerFactory.getLogger(AccessUtils.class);
 
     /**
@@ -40,7 +40,7 @@ public class AccessUtils {
     public static void validateIdentificationNumberAccess(RequestInfo requestInfo) throws ForbiddenException {
         if (requesterHasNoAccessRightToUseNationalIdentificationNumber(requestInfo)) {
             logger.warn(USER_DOES_NOT_HAVE_REQUIRED_ACCESS_RIGHT,
-                        requestInfo.getFeideId(), EDIT_OWN_INSTITUTION_USERS);
+                    requestInfo.getFeideId(), EDIT_OWN_INSTITUTION_USERS);
             throw new ForbiddenException();
         }
     }
@@ -54,7 +54,7 @@ public class AccessUtils {
     public static void verifyRequesterCanEditProjects(RequestInfo requestInfo) throws ForbiddenException {
         if (requesterHasNoAccessRightToEditProjects(requestInfo)) {
             logger.warn(USER_DOES_NOT_HAVE_REQUIRED_ACCESS_RIGHT,
-                        requestInfo.getFeideId(), EDIT_OWN_INSTITUTION_PROJECTS);
+                    requestInfo.getFeideId(), EDIT_OWN_INSTITUTION_PROJECTS);
             throw new ForbiddenException();
         }
     }
@@ -66,19 +66,19 @@ public class AccessUtils {
 
     private static boolean requesterHasNoAccessRightToUseNationalIdentificationNumber(RequestInfo requestInfo) {
         return !(
-            requestInfo.getAccessRights().contains(EDIT_OWN_INSTITUTION_USERS)
-            || clientIsInternalBackend(requestInfo)
+                requestInfo.getAccessRights().contains(EDIT_OWN_INSTITUTION_USERS)
+                        || clientIsInternalBackend(requestInfo)
         );
     }
 
     private static boolean clientIsInternalBackend(RequestInfo requestInfo) {
         return Optional.ofNullable(requestInfo.getRequestContext())
-            .map(requestContext -> requestContext.get(AUTHORIZER_FIELD))
-            .map(authorizerNode -> authorizerNode.get(ACCESS_TOKEN_CLAIMS_FIELD))
-            .map(claims -> claims.get(ACCESS_TOKEN_CLAIMS_SCOPE_FIELD))
-            .map(JsonNode::textValue)
-            .filter(BACKEND_SCOPE_AS_DEFINED_IN_IDENTITY_SERVICE::equals)
-            .isPresent();
+                .map(requestContext -> requestContext.get(AUTHORIZER_FIELD))
+                .map(authorizerNode -> authorizerNode.get(ACCESS_TOKEN_CLAIMS_FIELD))
+                .map(claims -> claims.get(ACCESS_TOKEN_CLAIMS_SCOPE_FIELD))
+                .map(JsonNode::textValue)
+                .filter(BACKEND_SCOPE_AS_DEFINED_IN_IDENTITY_SERVICE::equals)
+                .isPresent();
     }
 
     private static boolean requesterHasNoAccessRightToEditProjects(RequestInfo requestInfo) {
@@ -90,10 +90,11 @@ public class AccessUtils {
             final var authorizationHeader = requestInfo.getHeaders().get(AUTHORIZATION);
             if (nonNull(authorizationHeader) && authorizationHeader.startsWith(BEARER)) {
                 var token = authorizationHeader.substring(BEARER.length() + 1, authorizationHeader.length());
-                var payload = new String(Base64.getUrlDecoder().decode(token.split(DOT_SEPARATOR)[PAYLOAD_SEGMENT]));
-                var accessRights = JsonUtils.dtoObjectMapper.readTree(new StringReader(payload))
-                        .get(CUSTOM_ACCESS_RIGHTS).asText();
-                return accessRights.contains(EDIT_OWN_INSTITUTION_USERS);
+                DecodedJWT jwt = decode(token);
+                if(new Date().after(jwt.getExpiresAt())) {
+                    return false;
+                }
+                return jwt.getClaim(CUSTOM_ACCESS_RIGHTS).asString().contains(EDIT_OWN_INSTITUTION_USERS);
             }
         } catch (Exception ignored) {
             logger.debug("No valid access information in request authorization header");
