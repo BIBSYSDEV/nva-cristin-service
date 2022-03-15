0eaabac16e534a587e9af1231577fb06053512d7
svenngjeto <svenn.gjeto@unit.no>
2022-03-15 11:12:12 +0100

    Simple SQL injection recognizer

--- cristin-commons/src/main/java/no/unit/nva/utils/SqlRecognizer.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/SqlRecognizer.java
@@ -1,60 +1,20 @@
 package no.unit.nva.utils;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SqlRecognizer {
 
-    private static final Set<String> suspicousStatements = Set.of("''", "--", ";","$","=");
-    private static Pattern pattern = Pattern.compile("[\\p{Alnum}+\\s+-/–'&:();'#”«»´’_\"“`„‘’!^‐]+",Pattern.UNICODE_CHARACTER_CLASS);
+    private static final Set<String> suspicousStatements = Set.of("''", "--", ";", "$", "=");
 
-
-//    private static final Set<String> sqlStatements = Set.of("'",
-//            "and",
-//            "exec",
-//            "insert",
-//            "select",
-//            "delete",
-//            "update",
-//            "create",
-//            "alter",
-//            "count",
-//            "drop",
-//            "chr",
-//            "mid",
-//            "master",
-//            "truncate",
-//            "char",
-//            "declare",
-//            ";",
-//            "or",
-//            "-",
-//            ",");
-//
     /**
      * Checks if there are possible SQL statements in a string.
-     * @param str String to checked for SQL statements
+     *
+     * @param query String to checked for SQL statements
      * @return true if there are possible SQL statements in str
      */
-    public static boolean containsSqlInjectionFragmentCandidate(String str) {
-        Set<String> queryFragments  = new HashSet(List.of(str.toLowerCase(Locale.getDefault()).split(" ")));
-        Set intersect = queryFragments.stream().filter(suspicousStatements::contains).collect(Collectors.toSet());
+    public static boolean containsSqlInjectionFragmentCandidate(String query) {
+        Set intersect = suspicousStatements.stream().filter(query::contains).collect(Collectors.toSet());
         return !intersect.isEmpty();
-    }
-
-    public static boolean containsSuspicousInjectionFragmentCandidate(String str) {
-        Set<String> queryFragments  = new HashSet(List.of(str.toLowerCase(Locale.getDefault()).split("\\s")));
-        Set intersect = queryFragments.stream().filter(suspicousStatements::contains).collect(Collectors.toSet());
-
-        // |\\w|\\s+|'\\-'|'\\,','\\/','\\.')+"
-//        Pattern pattern = Pattern.compile("([p{Alpha}]+|\\w+)+");
-        final boolean onlyLegalCharacters = pattern.matcher(str).matches();
-        final boolean empty = intersect.isEmpty();
-        final boolean result = !empty || !onlyLegalCharacters;
-        return result;
     }
 }
--- cristin-commons/src/test/java/no/unit/nva/utils/SqlRecognizerTest.java
+++ cristin-commons/src/test/java/no/unit/nva/utils/SqlRecognizerTest.java
@@ -1,25 +1,16 @@
 package no.unit.nva.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import no.unit.nva.cristin.model.CristinUnit;
-import nva.commons.core.ioutils.IoUtils;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static no.unit.nva.cristin.model.Constants.OBJECT_MAPPER;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SqlRecognizerTest {
-
-    private static final String INSTITUTIONS_JSON = "institutions.json";
 
     @Test
     void legalQueryPassThru() {
@@ -46,41 +37,15 @@ class SqlRecognizerTest {
     }
 
     @Test
-    void checkNamesQuery() throws JsonProcessingException {
-        CristinUnit[] units = OBJECT_MAPPER.readValue(IoUtils.stringFromResources(Path.of(INSTITUTIONS_JSON)), CristinUnit[].class);
-        assertNotNull(units);
-        List<String> names = new ArrayList<>();
-        for (CristinUnit unit : units) {
-            names.addAll(unit.getUnitName().entrySet()
-                    .stream()
-                    .map(Map.Entry::getValue)
-                    .filter(SqlRecognizer::containsSuspicousInjectionFragmentCandidate)
-                    .collect(Collectors.toList()));
-            names.stream().filter(Objects::nonNull).forEach(System.out::println);
-        }
-    }
-
-    @Test
     void checkStatements() {
         List<String> candidates = List.of("Kémerovskij gosudárstvennyj universitét");
         List<String> names = new ArrayList<>();
         names.addAll(candidates.stream()
-                .filter(SqlRecognizer::containsSuspicousInjectionFragmentCandidate)
+                .filter(SqlRecognizer::containsSqlInjectionFragmentCandidate)
                 .collect(Collectors.toList()));
-
         names.stream().filter(Objects::nonNull).forEach(System.out::println);
     }
 }
-
-
-
-//    @Test
-//    void checkNames2Query() throws JsonProcessingException {
-//        JsonNode units = OBJECT_MAPPER.readTree(IoUtils.stringFromResources(Path.of(INSTITUTIONS_JSON)));
-//        units.findValuesAsText("unit_name").stream().forEach(System.out::println);
-//
-//        assertNotNull(units);
-//    }
 
 
 
