20d7b1fe32c070d5360757ee98c3aa15fa70ead3
svenngjeto <svenn.gjeto@unit.no>
2022-03-28 09:57:57 +0200

    refactor DeleteEmployment after QA

--- cristin-person/src/main/java/no/unit/nva/cristin/person/employment/delete/DeletePersonEmploymentClient.java
+++ cristin-person/src/main/java/no/unit/nva/cristin/person/employment/delete/DeletePersonEmploymentClient.java
@@ -1,39 +1,28 @@
 package no.unit.nva.cristin.person.employment.delete;
 
-import no.unit.nva.cristin.model.SearchResponse;
-import no.unit.nva.cristin.person.employment.query.QueryPersonEmploymentClient;
-import no.unit.nva.cristin.person.model.cristin.CristinPersonEmployment;
+import no.unit.nva.cristin.common.client.ApiClient;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.BadRequestException;
 import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.paths.UriWrapper;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 
 import static no.unit.nva.cristin.model.Constants.CRISTIN_API_URL;
 import static no.unit.nva.cristin.model.Constants.PERSON_PATH;
-import static nva.commons.core.attempt.Try.attempt;
+import static no.unit.nva.cristin.person.employment.query.QueryPersonEmploymentClient.EMPLOYMENT_PATH_CRISTIN;
 
-public class DeletePersonEmploymentClient extends QueryPersonEmploymentClient {
-    public static final int FIRST_HIT = 0;
-    private static final int SINGLE_HIT = 1;
-    public static final String EMPLOYMENT_NOT_FOUND = "Employment not found";
-    public static final String PERSON_NOT_FOUND = "Person not found";
-    public static final String PERSON_ID_NOT_UNIQUE = "PersonId not unique";
+public class DeletePersonEmploymentClient extends ApiClient {
+    public static final String BAD_REQUEST_FROM_UPSTREAM = "Upstream returned Bad Request. This might occur if "
+            + "person identifier or employment identifier are invalid";
 
-    /**
-     * Create CristinPersonApiClient with default HTTP client.
-     */
-    public DeletePersonEmploymentClient() {
-        this(HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .connectTimeout(Duration.ofSeconds(30))
-                .build());
-    }
+    public static final String NOT_FOUND_REQUEST_FROM_UPSTREAM = "Upstream returned Not Found. This might occur if "
+            + "person identifier or employment identifier are not found in upstream";
+
 
     public DeletePersonEmploymentClient(HttpClient client) {
         super(client);
@@ -43,21 +32,13 @@ public class DeletePersonEmploymentClient extends QueryPersonEmploymentClient {
      * Deletes a identified persons specified employment.
      * @param personId identification of person
      * @param employmentId identification of employment
-     * @return empty response ig successful
+     * @return empty response if successful
      * @throws ApiGatewayException when service encounters problems or user is not authorized.
      */
     public Void deletePersonEmployment(String personId, String employmentId) throws ApiGatewayException {
-        SearchResponse<CristinPersonEmployment> searchResponse =
-                attempt(() -> generateQueryResponse(personId)).orElseThrow();
-        validateResponse(searchResponse);
-        CristinPersonEmployment cristinPersonEmployment =
-                (CristinPersonEmployment) searchResponse.getHits().get(FIRST_HIT);
-        if (searchResponse.getHits().isEmpty() || !employmentId.equals(cristinPersonEmployment.getId())) {
-            throw new NotFoundException(EMPLOYMENT_NOT_FOUND);
-        }
         URI cristinUri = generateCristinUri(personId, employmentId);
         HttpResponse<String> response = deleteResults(cristinUri);
-        checkResponseForBadRequestIndicatingNotFoundIdentifier(response.statusCode());
+        checkResponse(response.statusCode());
         return null;
     }
 
@@ -70,12 +51,12 @@ public class DeletePersonEmploymentClient extends QueryPersonEmploymentClient {
                 .getUri();
     }
 
-    private void validateResponse(SearchResponse<CristinPersonEmployment> response) throws ApiGatewayException {
-        if (response.getHits().isEmpty()) {
-            throw new NotFoundException(PERSON_NOT_FOUND);
+    protected void checkResponse(int statusCode) throws ApiGatewayException {
+        if (statusCode == HttpURLConnection.HTTP_BAD_REQUEST) {
+            throw new BadRequestException(BAD_REQUEST_FROM_UPSTREAM);
         }
-        if (SINGLE_HIT != response.getHits().size()) {
-            throw new BadRequestException(PERSON_ID_NOT_UNIQUE);
+        if (statusCode == HttpURLConnection.HTTP_NOT_FOUND) {
+            throw new NotFoundException(NOT_FOUND_REQUEST_FROM_UPSTREAM);
         }
     }
 
