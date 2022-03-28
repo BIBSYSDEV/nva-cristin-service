1bb31b1b7b4ff6b7fe66f612249f3baad663a77c
svenngjeto <svenn.gjeto@unit.no>
2022-03-28 17:14:03 +0200

    Serializing Instants with zero millis

---
+++ cristin-commons/src/main/java/no/unit/nva/utils/CustomInstantSerializer.java
@@ -0,0 +1,31 @@
+package no.unit.nva.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class CustomInstantSerializer extends StdSerializer<Instant> {
+
+    private static DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendInstant(3).toFormatter();
+
+    public CustomInstantSerializer() {
+        this(null);
+    }
+
+    public CustomInstantSerializer(Class<Instant> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Instant value, JsonGenerator jsonGenerator, SerializerProvider arg2) throws IOException {
+        jsonGenerator.writeString(formatter.format(value));
+    }
+}
+
+
+
