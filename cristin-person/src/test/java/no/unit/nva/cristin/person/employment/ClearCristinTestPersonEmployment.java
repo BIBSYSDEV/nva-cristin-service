d0b35c58b5e56327d155763463af699423bd557b
torarnet <6398417+torarnet@users.noreply.github.com>
2022-03-30 15:36:06 +0200

    NP-3557: Refactor class to use new model

--- cristin-person/src/test/java/no/unit/nva/cristin/person/employment/ClearCristinTestPersonEmployment.java
+++ cristin-person/src/test/java/no/unit/nva/cristin/person/employment/ClearCristinTestPersonEmployment.java
@@ -1,10 +1,11 @@
 package no.unit.nva.cristin.person.employment;
 
+import static no.unit.nva.utils.UriUtils.extractLastPathElement;
 import no.unit.nva.cristin.common.client.CristinAuthenticator;
 import no.unit.nva.cristin.model.SearchResponse;
 import no.unit.nva.cristin.person.employment.delete.DeletePersonEmploymentClient;
 import no.unit.nva.cristin.person.employment.query.QueryPersonEmploymentClient;
-import no.unit.nva.cristin.person.model.cristin.CristinPersonEmployment;
+import no.unit.nva.cristin.person.model.nva.Employment;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,17 +22,17 @@ public class ClearCristinTestPersonEmployment {
      * @throws ApiGatewayException an error occurred during execution
      */
     public static void clearEmployment(String personId) throws ApiGatewayException {
-        SearchResponse<CristinPersonEmployment>  queryResponse =
-                new QueryPersonEmploymentClient(CristinAuthenticator.getHttpClient()).generateQueryResponse(personId);
-        List<CristinPersonEmployment> hits = (List<CristinPersonEmployment>) queryResponse.getHits();
+        SearchResponse<Employment> queryResponse =
+            new QueryPersonEmploymentClient(CristinAuthenticator.getHttpClient()).generateQueryResponse(personId);
+        List<Employment> hits = (List<Employment>) queryResponse.getHits();
         hits.forEach(hit -> deleteEmployment(personId, hit));
     }
 
-    private static void deleteEmployment(String personId, CristinPersonEmployment cristinPersonEmployment) {
+    private static void deleteEmployment(String personId, Employment employment) {
         try {
             new DeletePersonEmploymentClient(CristinAuthenticator.getHttpClient())
-                    .deletePersonEmployment(personId, cristinPersonEmployment.getId());
-            logger.info("Deleted employment {} for person {}", cristinPersonEmployment.getId(), personId);
+                .deletePersonEmployment(personId, extractLastPathElement(employment.getId()));
+            logger.info("Deleted employment {}", employment.getId());
         } catch (ApiGatewayException e) {
             logger.error("Exception deleting employment for person with id '{}'", personId, e);
         }
