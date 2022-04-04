2c61e8683b88e01bd9390c0b0e771b0ab1c3d0f0
svenngjeto <svenn.gjeto@unit.no>
2022-04-04 16:40:51 +0200

    Encoding space

--- cristin-project/src/main/java/no/unit/nva/cristin/projects/CristinApiClient.java
+++ cristin-project/src/main/java/no/unit/nva/cristin/projects/CristinApiClient.java
@@ -203,7 +203,7 @@ public class CristinApiClient extends ApiClient {
         }
 
         if (parameters.containsKey(STATUS)) {
-            query = query.withStatus(parameters.get(STATUS));
+            query = query.withStatus(URLEncoder.encode(parameters.get(STATUS),StandardCharsets.UTF_8));
         }
 
         return queryType == QUERY_USING_GRANT_ID ? query.withGrantId(parameters.get(QUERY)).toURI() :
