feb93e545eda36f8a867dcb4e0c7a60b6fde4546
RanjithTom <ranjithtomy@gmail.com>
2022-04-29 13:49:05 +0200

    Formatting

--- cristin-person/src/main/java/no/unit/nva/cristin/person/affiliations/client/CristinPositionCodesClient.java
+++ cristin-person/src/main/java/no/unit/nva/cristin/person/affiliations/client/CristinPositionCodesClient.java
@@ -80,8 +80,8 @@ public class CristinPositionCodesClient extends ApiClient {
     private List<CristinPositionCode> filterPositionCodeIfRequested(List<CristinPositionCode> cristinPositionCodes,
                                                                     Boolean positionStatus) {
         return cristinPositionCodes.stream()
-            .filter(position -> positionStatus == null ||
-                                positionStatus.equals(position.isEnabled()))
+            .filter(position -> positionStatus == null
+                                || positionStatus.equals(position.isEnabled()))
             .collect(Collectors.toList());
     }
 
