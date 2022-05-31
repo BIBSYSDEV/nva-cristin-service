118fd06dcfb36ae8f4bb370e7ec3615d9276b492
torarnet <6398417+torarnet@users.noreply.github.com>
2022-05-31 17:26:41 +0200

    NP-9150: Added unit test for sorting logic

--- cristin-person/src/test/java/no/unit/nva/cristin/person/handler/ListCristinOrganizationPersonsHandlerTest.java
+++ cristin-person/src/test/java/no/unit/nva/cristin/person/handler/ListCristinOrganizationPersonsHandlerTest.java
@@ -5,6 +5,7 @@ import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_INVALID_PATH_PARAMETER_FOR_ID_FOUR_NUMBERS;
 import static no.unit.nva.cristin.common.ErrorMessages.validQueryParameterNamesMessage;
+import static no.unit.nva.cristin.model.Constants.OBJECT_MAPPER;
 import static no.unit.nva.cristin.model.JsonPropertyNames.IDENTIFIER;
 import static no.unit.nva.cristin.model.JsonPropertyNames.NAME;
 import static no.unit.nva.cristin.model.JsonPropertyNames.NUMBER_OF_RESULTS;
@@ -25,21 +26,27 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Comparators;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import no.unit.nva.commons.json.JsonUtils;
 import no.unit.nva.cristin.model.SearchResponse;
 import no.unit.nva.cristin.person.client.CristinOrganizationPersonsClient;
+import no.unit.nva.cristin.person.model.cristin.CristinPerson;
 import no.unit.nva.cristin.person.model.nva.Person;
 import no.unit.nva.cristin.testing.HttpResponseFaker;
 import no.unit.nva.testutils.HandlerRequestBuilder;
+import no.unit.nva.utils.UriUtils;
 import nva.commons.apigateway.GatewayResponse;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.core.Environment;
@@ -144,6 +151,56 @@ class ListCristinOrganizationPersonsHandlerTest {
         assertThat(Optional.ofNullable(responseBody.getId()).toString(),
                    containsString(NAME + EQUAL_OPERATOR + name));
         assertEquals(HTTP_OK, gatewayResponse.getStatusCode());
+    }
+
+    @Test
+    void shouldSortResultsBasedOnPersonIdentifier() throws IOException, ApiGatewayException {
+        var fakeHttpResponse = generateCristinResponseWithDifferentIdentifiers();
+        cristinApiClient = spy(cristinApiClient);
+        doReturn(fakeHttpResponse).when(cristinApiClient).fetchQueryResults(any());
+        handler = new ListCristinOrganizationPersonsHandler(cristinApiClient, new Environment());
+        var input = generateHandlerDummyRequest();
+        handler.handleRequest(input, output, context);
+        var gatewayResponse = GatewayResponse.fromOutputStream(output,
+                                                               SearchResponse.class);
+        var responseBody = gatewayResponse.getBodyObject(SearchResponse.class);
+        var hits = extractHitsFromSearchResponse(responseBody);
+        var isOrdered = Comparators.isInOrder(hits, compareCristinIdentifiersDesc());
+
+        assertThat(isOrdered, equalTo(true));
+        assertEquals(HTTP_OK, gatewayResponse.getStatusCode());
+    }
+
+    private HttpResponseFaker generateCristinResponseWithDifferentIdentifiers() throws JsonProcessingException {
+        var cristinResponse = restApiMapper.createArrayNode();
+        cristinResponse.addPOJO(generateCristinPerson(20, randomString()));
+        cristinResponse.addPOJO(generateCristinPerson(30, randomString()));
+        cristinResponse.addPOJO(generateCristinPerson(10, randomString()));
+        return new HttpResponseFaker(restApiMapper.writeValueAsString(cristinResponse), 200);
+    }
+
+    private Comparator<Person> compareCristinIdentifiersDesc() {
+        return Comparator.comparing(this::extractCristinIdentifierFromPerson).reversed();
+    }
+
+    private Integer extractCristinIdentifierFromPerson(Person person) {
+        return Optional.ofNullable(person)
+                   .map(Person::getId)
+                   .map(UriUtils::extractLastPathElement)
+                   .map(Integer::valueOf)
+                   .orElseThrow();
+    }
+
+    private CristinPerson generateCristinPerson(int identifier, String name) {
+        var cristinPerson = new CristinPerson();
+        cristinPerson.setCristinPersonId(String.valueOf(identifier));
+        cristinPerson.setFirstName(name);
+        return cristinPerson;
+    }
+
+    private List<Person> extractHitsFromSearchResponse(SearchResponse response) {
+        return OBJECT_MAPPER.convertValue(response.getHits(), new TypeReference<>() {
+        });
     }
 
     private InputStream queryWithNameParam(String name) throws JsonProcessingException {
