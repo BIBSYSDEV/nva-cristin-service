8bee8b1c64310ff191439cf7b334e52b83a6149e
torarnet <6398417+torarnet@users.noreply.github.com>
2022-11-07 13:40:21 +0100

    NP-17310: removed redundant tests

--- cristin-project/src/test/java/no/unit/nva/cristin/projects/CristinProjectBuilderTest.java
+++ cristin-project/src/test/java/no/unit/nva/cristin/projects/CristinProjectBuilderTest.java
@@ -3,21 +3,14 @@ package no.unit.nva.cristin.projects;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
-import no.unit.nva.cristin.projects.model.cristin.CristinContactInfo;
-import no.unit.nva.cristin.projects.model.cristin.CristinFundingAmount;
 import no.unit.nva.cristin.projects.model.cristin.CristinProject;
-import no.unit.nva.cristin.projects.model.cristin.CristinTypedLabel;
 import no.unit.nva.cristin.projects.model.nva.NvaProject;
 import org.junit.jupiter.api.Test;
 
 import static no.unit.nva.cristin.model.Constants.OBJECT_MAPPER;
-import static no.unit.nva.cristin.projects.NvaProjectBuilderTest.CONTACT_EMAIL;
 import static no.unit.nva.cristin.projects.RandomProjectDataGenerator.IGNORE_LIST;
-import static no.unit.nva.cristin.projects.RandomProjectDataGenerator.randomNamesMap;
 import static no.unit.nva.cristin.projects.RandomProjectDataGenerator.randomNvaProject;
 import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValuesIgnoringFields;
-import static no.unit.nva.testutils.RandomDataGenerator.randomInteger;
-import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static nva.commons.core.attempt.Try.attempt;
 import static nva.commons.core.ioutils.IoUtils.stringFromResources;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -44,36 +37,6 @@ class CristinProjectBuilderTest {
 
         assertThat(actual, doesNotHaveEmptyValuesIgnoringFields(ignored));
         assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldSerializeAndDeserializeCristinContactInfoIntoSameObject() {
-        var cristinContactInfo = new CristinContactInfo(randomString(), randomString(), CONTACT_EMAIL, randomString());
-        var serialized = attempt(() -> OBJECT_MAPPER.writeValueAsString(cristinContactInfo)).orElseThrow();
-        var deserialized =
-            attempt(() -> OBJECT_MAPPER.readValue(serialized, CristinContactInfo.class)).orElseThrow();
-
-        assertThat(deserialized, equalTo(cristinContactInfo));
-    }
-
-    @Test
-    void shouldSerializeAndDeserializeCristinFundingAmountIntoSameObject() {
-        var cristinFundingAmount = new CristinFundingAmount(randomString(), randomInteger().doubleValue());
-        var serialized = attempt(() -> OBJECT_MAPPER.writeValueAsString(cristinFundingAmount)).orElseThrow();
-        var deserialized =
-            attempt(() -> OBJECT_MAPPER.readValue(serialized, CristinFundingAmount.class)).orElseThrow();
-
-        assertThat(deserialized, equalTo(cristinFundingAmount));
-    }
-
-    @Test
-    void shouldSerializeAndDeserializeCristinTypedLabelIntoSameObject() {
-        var cristinTypedLabel = new CristinTypedLabel(randomString(), randomNamesMap());
-        var serialized = attempt(() -> OBJECT_MAPPER.writeValueAsString(cristinTypedLabel)).orElseThrow();
-        var deserialized =
-            attempt(() -> OBJECT_MAPPER.readValue(serialized, CristinTypedLabel.class)).orElseThrow();
-
-        assertThat(deserialized, equalTo(cristinTypedLabel));
     }
 
     @Test
