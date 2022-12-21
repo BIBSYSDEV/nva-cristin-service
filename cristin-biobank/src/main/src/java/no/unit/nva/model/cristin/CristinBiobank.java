5dfdb9ba65e71ba83e883bc21df49b0371b5eb91
Marina Atkina <marina.atkina@tietoevry.com>
2022-12-21 11:57:40 +0100

    Small fix for model

--- cristin-biobank/src/main/src/java/no/unit/nva/model/cristin/CristinBiobank.java
+++ cristin-biobank/src/main/src/java/no/unit/nva/model/cristin/CristinBiobank.java
@@ -5,18 +5,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public final class CristinBiobank {
     //Here be more params
     private static final String CODE_FIELD = "code";
+    private static final String NAME_FIELD = "name";
 
     @JsonProperty(CODE_FIELD)
     private final String code;
+    @JsonProperty(NAME_FIELD)
+    private final Map<String, String> name;
 
 
-    public CristinBiobank(@JsonProperty(CODE_FIELD) String code) {
+    public CristinBiobank(@JsonProperty(CODE_FIELD) String code,
+                          @JsonProperty(NAME_FIELD) Map<String, String> name) {
         this.code = code;
-
+        this.name  = Collections.unmodifiableMap(name);;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public Map<String, String> getName() {
+        return name;
     }
 
 }
