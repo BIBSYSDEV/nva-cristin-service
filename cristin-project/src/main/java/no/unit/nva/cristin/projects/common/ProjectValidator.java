a9ada189dbf3d18b7fa83f5bd6d74ce8130f4293
torarnet <6398417+torarnet@users.noreply.github.com>
2023-01-18 13:35:50 +0100

    NP-16247: Changed interface

--- cristin-project/src/main/java/no/unit/nva/cristin/projects/common/ProjectValidator.java
+++
@@ -1,9 +0,0 @@
-package no.unit.nva.cristin.projects.common;
-
-import nva.commons.apigateway.exceptions.ApiGatewayException;
-
-public interface ProjectValidator {
-
-    <T> void validate(T classOfT) throws ApiGatewayException;
-
-}
