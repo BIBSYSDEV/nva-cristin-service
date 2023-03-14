bfb0b5f9348ffd80999d9e2803d0c6778354a357
torarnet <6398417+torarnet@users.noreply.github.com>
2023-03-14 10:41:37 +0100

    np-32448: WiP

--- cristin-project/src/main/java/no/unit/nva/cristin/projects/create/CreateCristinProjectHandler.java
+++ cristin-project/src/main/java/no/unit/nva/cristin/projects/create/CreateCristinProjectHandler.java
@@ -2,9 +2,13 @@ package no.unit.nva.cristin.projects.create;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.common.net.MediaType;
+import java.net.URI;
 import no.unit.nva.Validator;
 import no.unit.nva.cristin.common.client.CristinAuthenticator;
+import no.unit.nva.cristin.projects.model.nva.NvaContributor;
 import no.unit.nva.cristin.projects.model.nva.NvaProject;
+import no.unit.nva.cristin.projects.model.nva.Person;
+import no.unit.nva.model.Organization;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.RestRequestHandler;
@@ -13,11 +17,22 @@ import nva.commons.core.Environment;
 
 import java.net.HttpURLConnection;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static no.unit.nva.cristin.model.Constants.BASE_PATH;
 import static no.unit.nva.cristin.model.Constants.DEFAULT_RESPONSE_MEDIA_TYPES;
+import static no.unit.nva.cristin.model.Constants.DOMAIN_NAME;
+import static no.unit.nva.cristin.model.Constants.PERSON_PATH_NVA;
 import static no.unit.nva.utils.AccessUtils.verifyRequesterCanEditProjects;
+import static no.unit.nva.utils.LogUtils.LOG_IDENTIFIERS;
+import static no.unit.nva.utils.LogUtils.extractCristinIdentifier;
+import static no.unit.nva.utils.LogUtils.extractOrgIdentifier;
+import static nva.commons.core.paths.UriWrapper.fromUri;
 
 public class CreateCristinProjectHandler extends ApiGatewayHandler<NvaProject, NvaProject> {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateCristinProjectHandler.class);
 
     private final transient CreateCristinProjectApiClient apiClient;
 
@@ -56,10 +71,37 @@ public class CreateCristinProjectHandler extends ApiGatewayHandler<NvaProject, N
             throws ApiGatewayException {
 
         verifyRequesterCanEditProjects(requestInfo);
+        logger.info(LOG_IDENTIFIERS, extractCristinIdentifier(requestInfo), extractOrgIdentifier(requestInfo));
         Validator<NvaProject> validator = new CreateCristinProjectValidator();
         validator.validate(input);
+        addRegistrarDataToInput(input, requestInfo);
 
         return apiClient.createProjectInCristin(input);
+    }
+
+    private void addRegistrarDataToInput(NvaProject input, RequestInfo requestInfo) {
+        var registrar = new NvaContributor();
+        registrar.setIdentity(getIdentity(requestInfo));
+        registrar.setAffiliation(getAffiliation(requestInfo));
+
+        input.getContributors().add(registrar);
+    }
+
+    private Organization getAffiliation(RequestInfo requestInfo) {
+        var affiliation = createIdUriFromRequest(extractOrgIdentifier(requestInfo));
+        return new Organization.Builder().withId(affiliation).build();
+    }
+
+    private Person getIdentity(RequestInfo requestInfo) {
+        var identity = createIdUriFromRequest(extractCristinIdentifier(requestInfo));
+        return new Person.Builder().withId(identity).build();
+    }
+
+    private URI createIdUriFromRequest(String identifier) {
+        return fromUri(DOMAIN_NAME)
+                   .addChild(BASE_PATH)
+                   .addChild(PERSON_PATH_NVA)
+                   .addChild(identifier).getUri();
     }
 
     /**
