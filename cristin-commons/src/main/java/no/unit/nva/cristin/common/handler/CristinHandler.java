b8480a04db46c56c8f46da3bbbb25413f273f0ca
torarnet <6398417+torarnet@users.noreply.github.com>
2023-03-21 14:10:04 +0100

    np-39239: removed now redundant code

--- cristin-commons/src/main/java/no/unit/nva/cristin/common/handler/CristinHandler.java
+++ cristin-commons/src/main/java/no/unit/nva/cristin/common/handler/CristinHandler.java
@@ -3,22 +3,16 @@ package no.unit.nva.cristin.common.handler;
 import com.google.common.net.MediaType;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
-import nva.commons.apigateway.exceptions.BadRequestException;
 import nva.commons.core.Environment;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_INVALID_VALUE;
 import static no.unit.nva.cristin.model.Constants.DEFAULT_RESPONSE_MEDIA_TYPES;
-import static no.unit.nva.cristin.model.JsonPropertyNames.LANGUAGE;
-import static nva.commons.core.attempt.Try.attempt;
 
 public abstract class CristinHandler<I, O> extends ApiGatewayHandler<I, O> {
 
     public static final String DEFAULT_LANGUAGE_CODE = "nb";
-    private static final Set<String> VALID_LANGUAGE_CODES = Set.of("en", "nb", "nn");
 
     public CristinHandler(Class<I> iclass, Environment environment) {
         super(iclass, environment);
@@ -29,15 +23,18 @@ public abstract class CristinHandler<I, O> extends ApiGatewayHandler<I, O> {
         return DEFAULT_RESPONSE_MEDIA_TYPES;
     }
 
-    protected static String getValidLanguage(RequestInfo requestInfo) throws BadRequestException {
-        return Optional.of(getQueryParameter(requestInfo, LANGUAGE)
-            .orElse(DEFAULT_LANGUAGE_CODE))
-            .filter(VALID_LANGUAGE_CODES::contains)
-            .orElseThrow(() -> new BadRequestException(String.format(ERROR_MESSAGE_INVALID_VALUE, LANGUAGE)));
-    }
-
+    /**
+     * Get query parameter if present
+     * @deprecated
+     * This method is no longer needed. Is replaced by another.
+     * <p> Use {@link RequestInfo#getQueryParameterOpt(String)} instead.
+     *
+     * @param queryParameter param name
+     * @return param value if present
+     */
+    @Deprecated
     protected static Optional<String> getQueryParameter(RequestInfo requestInfo, String queryParameter) {
-        return attempt(() -> requestInfo.getQueryParameter(queryParameter)).toOptional();
+        return requestInfo.getQueryParameterOpt(queryParameter);
     }
 
 }
