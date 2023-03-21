36e0750e45e6ac3e64aba0693e28966aadf9d0fa
torarnet <6398417+torarnet@users.noreply.github.com>
2023-03-21 15:33:44 +0100

    np-39239: remove deprecated method

--- cristin-commons/src/main/java/no/unit/nva/cristin/common/handler/CristinHandler.java
+++ cristin-commons/src/main/java/no/unit/nva/cristin/common/handler/CristinHandler.java
@@ -2,11 +2,9 @@ package no.unit.nva.cristin.common.handler;
 
 import com.google.common.net.MediaType;
 import nva.commons.apigateway.ApiGatewayHandler;
-import nva.commons.apigateway.RequestInfo;
 import nva.commons.core.Environment;
 
 import java.util.List;
-import java.util.Optional;
 
 import static no.unit.nva.cristin.model.Constants.DEFAULT_RESPONSE_MEDIA_TYPES;
 
@@ -21,20 +19,6 @@ public abstract class CristinHandler<I, O> extends ApiGatewayHandler<I, O> {
     @Override
     protected List<MediaType> listSupportedMediaTypes() {
         return DEFAULT_RESPONSE_MEDIA_TYPES;
-    }
-
-    /**
-     * Get query parameter if present
-     * @deprecated
-     * This method is no longer needed. Is replaced by another.
-     * <p> Use {@link RequestInfo#getQueryParameterOpt(String)} instead.
-     *
-     * @param queryParameter param name
-     * @return param value if present
-     */
-    @Deprecated
-    protected static Optional<String> getQueryParameter(RequestInfo requestInfo, String queryParameter) {
-        return requestInfo.getQueryParameterOpt(queryParameter);
     }
 
 }
