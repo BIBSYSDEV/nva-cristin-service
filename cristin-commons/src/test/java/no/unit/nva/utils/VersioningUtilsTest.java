fd30638509644da35a879a8c353c3a68bb663210
torarnet <6398417+torarnet@users.noreply.github.com>
2023-05-24 14:29:18 +0200

    np-44759: test for quoted versioning

--- cristin-commons/src/test/java/no/unit/nva/utils/VersioningUtilsTest.java
+++ cristin-commons/src/test/java/no/unit/nva/utils/VersioningUtilsTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 class VersioningUtilsTest {
 
     public static final String ACCEPT_HEADER_VALUE = "application/json; version=2023-05-10";
+    public static final String ACCEPT_HEADER_VALUE_WITH_QUOTES = "application/json; version=\"2023-05-10\"";
     public static final String ACCEPT_HEADER_VALUE_WITHOUT_VERSION = "application/json";
     public static final String VERSION_VALUE = "2023-05-10";
 
@@ -23,6 +24,13 @@ class VersioningUtilsTest {
         var actual = extractVersion(ACCEPT_HEADER_VALUE_WITHOUT_VERSION);
 
         assertThat(actual, equalTo(null));
+    }
+
+    @Test
+    void shouldExtractVersionFromHeaderWhenSurroundedWithQuotes() {
+        var actual = extractVersion(ACCEPT_HEADER_VALUE_WITH_QUOTES);
+
+        assertThat(actual, equalTo(VERSION_VALUE));
     }
 
 }
