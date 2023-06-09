d23fc5166000df738de5b7b8198201cacc57a2c7
torarnet <6398417+torarnet@users.noreply.github.com>
2023-06-09 13:59:32 +0200

    np-41723: code feedback update

--- cristin-organization/src/main/java/no/unit/nva/cristin/organization/common/HandlerUtil.java
+++ cristin-organization/src/main/java/no/unit/nva/cristin/organization/common/HandlerUtil.java
@@ -1,11 +1,11 @@
 package no.unit.nva.cristin.organization.common;
 
+import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_DEPTH_INVALID;
 import static no.unit.nva.cristin.model.Constants.FULL;
 import static no.unit.nva.cristin.model.Constants.NONE;
 import static no.unit.nva.cristin.model.Constants.TOP;
 import static no.unit.nva.cristin.model.JsonPropertyNames.DEPTH;
 import java.util.Set;
-import no.unit.nva.cristin.common.ErrorMessages;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.BadRequestException;
 
@@ -17,19 +17,15 @@ public class HandlerUtil {
      * @param requestInfo Info from the request
      */
     public static String getValidDepth(RequestInfo requestInfo) throws BadRequestException {
-        if (isValidDepth(requestInfo)) {
-            return requestInfo.getQueryParameters().containsKey(DEPTH)
-                       ? requestInfo.getQueryParameter(DEPTH)
-                       : TOP;
-        } else {
-            throw new BadRequestException(ErrorMessages.ERROR_MESSAGE_DEPTH_INVALID);
-        }
+        var depth = requestInfo.getQueryParameterOpt(DEPTH).orElse(TOP);
+        validateDepth(depth);
+        return depth;
     }
 
-    private static boolean isValidDepth(RequestInfo requestInfo) throws BadRequestException {
-        return !requestInfo.getQueryParameters().containsKey(DEPTH)
-               || requestInfo.getQueryParameters().containsKey(DEPTH)
-                  && Set.of(TOP, FULL, NONE).contains(requestInfo.getQueryParameter(DEPTH));
+    private static void validateDepth(String depth) throws BadRequestException {
+        if (!Set.of(TOP, FULL, NONE).contains(depth)) {
+            throw new BadRequestException(ERROR_MESSAGE_DEPTH_INVALID);
+        }
     }
 
 }
