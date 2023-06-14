99a3baf6fdd1003568f4b16014b9a75b76a77c6c
torarnet <6398417+torarnet@users.noreply.github.com>
2023-06-14 14:39:14 +0200

    np-44869: javadoc

--- cristin-commons/src/main/java/no/unit/nva/utils/HandlerAccessCheck.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/HandlerAccessCheck.java
@@ -5,6 +5,12 @@ import nva.commons.apigateway.exceptions.ApiGatewayException;
 
 public interface HandlerAccessCheck {
 
+    /**
+     * Verifies if the user has access based on the request data.
+     *
+     * @param requestInfo data from the request used in access verification.
+     * @throws ApiGatewayException access control exceptions
+     **/
     void verifyAccess(RequestInfo requestInfo) throws ApiGatewayException;
 
 }
--- cristin-commons/src/main/java/no/unit/nva/utils/ResourceAccessCheck.java
+++ cristin-commons/src/main/java/no/unit/nva/utils/ResourceAccessCheck.java
@@ -4,6 +4,12 @@ import nva.commons.apigateway.exceptions.ApiGatewayException;
 
 public interface ResourceAccessCheck<T> {
 
+    /**
+     * Verifies if the user has access based on the resource requested access to.
+     *
+     * @param resource the resource to use in access verification.
+     * @throws ApiGatewayException access control exceptions
+     **/
     void verifyAccess(T resource) throws ApiGatewayException;
 
 }
