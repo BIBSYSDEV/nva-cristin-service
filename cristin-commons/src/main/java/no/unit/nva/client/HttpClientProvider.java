2149ca146192bf334ba4c782251842940a4b61b7
torarnet <6398417+torarnet@users.noreply.github.com>
2023-12-04 13:28:10 +0100

    np-45763: creating a default retry configuration. added dependencies

--- cristin-commons/src/main/java/no/unit/nva/client/HttpClientProvider.java
+++ cristin-commons/src/main/java/no/unit/nva/client/HttpClientProvider.java
@@ -1,8 +1,16 @@
 package no.unit.nva.client;
 
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Predicate;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
 
 public class HttpClientProvider {
 
@@ -15,6 +23,32 @@ public class HttpClientProvider {
                    .version(Version.HTTP_1_1)
                    .connectTimeout(Duration.ofSeconds(15))
                    .build();
+    }
+
+    /**
+     * Creates an object that helps in retrying failed http requests. Can be used when calling upstream.
+     */
+    public static RetryRegistry defaultRetryRegistry() {
+        return RetryRegistry.of(defaultRetryConfigFactory());
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static RetryConfig defaultRetryConfigFactory() {
+        return RetryConfig.<HttpResponse>custom()
+                   .maxAttempts(3)
+                   .waitDuration(Duration.ofMillis(100))
+                   .retryOnResult(resultStatusIsServerError())
+                   .retryExceptions(InterruptedException.class,
+                                    IOException.class,
+                                    TimeoutException.class,
+                                    HttpTimeoutException.class)
+                   .ignoreExceptions(ApiGatewayException.class)
+                   .build();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static Predicate<HttpResponse> resultStatusIsServerError() {
+        return httpResponse -> httpResponse.statusCode() >= 500 && httpResponse.statusCode() <= 599;
     }
 
 }
