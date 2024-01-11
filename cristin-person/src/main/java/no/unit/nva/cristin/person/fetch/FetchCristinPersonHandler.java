c81c24034de95035d5c1ba8952e64b44d8843ce6
torarnet <6398417+torarnet@users.noreply.github.com>
2024-01-11 10:20:10 +0100

    np-11602: employment karate test. add json-ld accept header (#351)

--- cristin-person/src/main/java/no/unit/nva/cristin/person/fetch/FetchCristinPersonHandler.java
+++ cristin-person/src/main/java/no/unit/nva/cristin/person/fetch/FetchCristinPersonHandler.java
@@ -1,6 +1,8 @@
 package no.unit.nva.cristin.person.fetch;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.google.common.net.MediaType;
+import java.util.List;
 import no.unit.nva.cristin.common.Utils;
 import no.unit.nva.cristin.person.client.CristinPersonApiClient;
 import no.unit.nva.cristin.person.model.nva.Person;
@@ -18,6 +20,7 @@ import java.net.HttpURLConnection;
 
 import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_INVALID_PATH_PARAMETER_FOR_PERSON_ID;
 import static no.unit.nva.cristin.common.ErrorMessages.ERROR_MESSAGE_INVALID_QUERY_PARAMETER_ON_PERSON_LOOKUP;
+import static no.unit.nva.cristin.model.Constants.DEFAULT_RESPONSE_MEDIA_TYPES;
 import static no.unit.nva.cristin.model.JsonPropertyNames.ID;
 import static no.unit.nva.utils.LogUtils.LOG_IDENTIFIERS;
 import static no.unit.nva.utils.LogUtils.extractCristinIdentifier;
@@ -67,6 +70,11 @@ public class FetchCristinPersonHandler extends ApiGatewayHandler<Void, Person> {
     @Override
     protected Integer getSuccessStatusCode(Void input, Person output) {
         return HttpURLConnection.HTTP_OK;
+    }
+
+    @Override
+    protected List<MediaType> listSupportedMediaTypes() {
+        return DEFAULT_RESPONSE_MEDIA_TYPES;
     }
 
     private void validateQueryParameters(RequestInfo requestInfo) throws BadRequestException {
