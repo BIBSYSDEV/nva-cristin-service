dfd238a68ebe97fb4d91e80e07e2bd39078ff6bf
torarnet <6398417+torarnet@users.noreply.github.com>
2024-03-08 15:19:42 +0100

    np-46360: removed unused constant

--- cristin-commons/src/main/java/no/unit/nva/cristin/model/JsonPropertyNames.java
+++ cristin-commons/src/main/java/no/unit/nva/cristin/model/JsonPropertyNames.java
@@ -19,7 +19,6 @@ public class JsonPropertyNames {
     public static final String CRISTIN_EMPLOYMENTS = "detailed_affiliations";
     public static final String CRISTIN_END_DATE = "end_date";
     public static final String CRISTIN_INSTITUTION_ID = "cristin_institution_id";
-    public static final String CRISTIN_QUERY_PARAMETER_LANGUAGE_KEY = "lang";
     public static final String CRISTIN_START_DATE = "start_date";
     public static final String CRISTIN_UNIT_ID = "cristin_unit_id";
     public static final String DEPTH = "depth";
