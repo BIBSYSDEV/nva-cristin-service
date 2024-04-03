744b5bd13d8856ab797c8a8b9bafcc59f7b935da
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2024-04-03 11:22:08 +0200

    feat: add cristin units intermediate s3 storage

---
+++ cristin-intermediate-storage/src/main/java/no/unit/nva/cristin/intermediate/storage/CristinUnitsUtil.java
@@ -0,0 +1,114 @@
+package no.unit.nva.cristin.intermediate.storage;
+
+import static io.vavr.control.Try.of;
+import static io.vavr.control.Try.ofSupplier;
+import static nva.commons.core.attempt.Try.attempt;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Supplier;
+import no.unit.nva.commons.json.JsonUtils;
+import nva.commons.core.Environment;
+import nva.commons.core.useragent.UserAgent;
+
+/**
+ * Utility class for retrieving all Norwegian cristin units where you want to do repeated lookups and keep a large
+ * cache like migrations and other bulk operations.
+ */
+public class CristinUnitsUtil {
+
+    private static final String API_ARGUMENTS = "?per_page=1000&country=NO&page=";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String ACCEPT = "Accept";
+    public static final String USER_AGENT = "User-Agent";
+    public static final String API_HOST = "CRISTIN_API_URL";
+    public static final String SIKT_EMIL = "support@sikt.no";
+    public static final URI GITHUB_REPO = URI.create("https://github.com/BIBSYSDEV/nva-publication-api");
+    private final URI apiUri;
+    private final HttpClient httpClient;
+    private final Environment environment;
+    private final Class<?> caller;
+    private final String cristinBotFilterBypassHeaderName;
+    private final String cristinBotFilterBypassHeaderValue;
+
+    public CristinUnitsUtil(HttpClient httpClient, URI apiUri, Environment environment, Class<?> caller,
+                            String cristinBotFilterBypassHeaderName, String cristinBotFilterBypassHeaderValue) {
+        this.httpClient = httpClient;
+        this.apiUri = apiUri;
+        this.environment = environment;
+        this.caller = caller;
+        this.cristinBotFilterBypassHeaderName = cristinBotFilterBypassHeaderName;
+        this.cristinBotFilterBypassHeaderValue = cristinBotFilterBypassHeaderValue;
+    }
+
+    public String getAllData() {
+        int pageNum = 1;
+        ArrayNode mergedArray = JsonUtils.dtoObjectMapper.createArrayNode();
+
+        while (true) {
+            var units = getApiData(pageNum);
+            if (units.isEmpty()) {
+                break;
+            } else {
+                mergedArray.addAll(units);
+                pageNum++;
+            }
+        }
+
+        return mergedArray.toString();
+    }
+
+    private ArrayNode getApiData(int pageNum) {
+        try {
+            var response = fetchResponseWithRetry(new URI(apiUri + API_ARGUMENTS + pageNum));
+            return attempt(() -> (ArrayNode) JsonUtils.dtoObjectMapper.readTree(response)).orElseThrow();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String fetchResponseWithRetry(URI requestUri) {
+        var retryRegistry = RetryRegistry.of(RetryConfig.custom().maxAttempts(5).intervalFunction(
+            IntervalFunction.ofExponentialRandomBackoff()).build());
+        var retryWithDefaultConfig = retryRegistry.retry("executeRequest");
+        Supplier<String> supplier = () -> executeRequest(requestUri);
+
+        return ofSupplier(Retry.decorateSupplier(retryWithDefaultConfig, supplier)).get();
+    }
+
+    private String executeRequest(URI requestUri) {
+        return of(() -> attempt(() -> httpClient.send(buildHttpRequest(requestUri),
+                                                      BodyHandlers.ofString(StandardCharsets.UTF_8)))
+                            .map(HttpResponse::body)
+                            .toOptional().orElseThrow()).get();
+    }
+
+    private HttpRequest buildHttpRequest(URI requestUri) {
+        return HttpRequest.newBuilder()
+                   .uri(requestUri)
+                   .headers(ACCEPT, APPLICATION_JSON,
+                            cristinBotFilterBypassHeaderName,
+                            cristinBotFilterBypassHeaderValue,
+                            USER_AGENT, getUserAgent())
+                   .GET()
+                   .build();
+    }
+
+    private String getUserAgent() {
+        return UserAgent.newBuilder().client(caller)
+                   .environment(this.environment.readEnv(API_HOST))
+                   .repository(GITHUB_REPO)
+                   .email(SIKT_EMIL)
+                   .version("1.0")
+                   .build().toString();
+    }
+}
---
+++ cristin-intermediate-storage/src/test/java/no/unit/nva/cristin/intermediate/storage/UpdateUnitsHandlerTest.java
@@ -0,0 +1,95 @@
+package no.unit.nva.cristin.intermediate.storage;
+
+import static no.unit.nva.cristin.model.Constants.OBJECT_MAPPER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import com.amazonaws.services.lambda.runtime.Context;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.core.Environment;
+import nva.commons.core.ioutils.IoUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.stubbing.Answer;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.http.ContentStreamProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+public class UpdateUnitsHandlerTest {
+    public static final String TEST_BUCKET = "test-bucket";
+    public static final String DELIMITER = "\\A";
+    public static final String UNITS_JSON_FILENAME = "units.json";
+    private Environment environment;
+    private S3Client s3Client;
+    private HttpClient httpClient;
+    private Context context;
+    private ByteArrayOutputStream output;
+    private InputStream input;
+
+    @BeforeEach
+    void setUp() throws IOException, InterruptedException {
+        this.environment = mock(Environment.class);
+        this.s3Client = mock(S3Client.class);
+        this.httpClient = mock(HttpClient.class);
+        this.context = mock(Context.class);
+        this.output = new ByteArrayOutputStream();
+        this.input = new HandlerRequestBuilder<Void>(OBJECT_MAPPER).build();
+        when(httpClient.send(any(HttpRequest.class),
+                             ArgumentMatchers.<HttpResponse.BodyHandler<String>>any())).thenAnswer(
+            (Answer<HttpResponse<String>>) invocationOnMock -> {
+                HttpRequest request = invocationOnMock.getArgument(0);
+                int lastDigit =
+                    Character.getNumericValue(request.uri().toString().charAt(request.uri().toString().length() - 1));
+                var result = IoUtils.stringFromResources(Path.of("cristinUnits/units%d.json".formatted(lastDigit)));
+                HttpResponse<String> httpResponse = mock(HttpResponse.class);
+                when(httpResponse.body()).thenReturn(result);
+                return httpResponse;
+            });
+
+        when(environment.readEnv("CRISTIN_API_URL")).thenReturn("https://api.unittest.nva.aws.sikt.no");
+        when(environment.readEnv("INTERMEDIATE_STORAGE_BUCKET_NAME")).thenReturn(TEST_BUCKET);
+        when(environment.readEnv("CRISTIN_BOT_FILTER_BYPASS_HEADER_NAME")).thenReturn("cristinBotFilterBypassHeaderName");
+        when(environment.readEnv("CRISTIN_BOT_FILTER_BYPASS_HEADER_VALUE")).thenReturn("cristinBotFilterBypassHeaderValue");
+    }
+
+    @Test
+    void unitCachedFileShouldUpdate() throws IOException {
+        var handler = new UpdateUnitsHandler(environment, s3Client, httpClient);
+
+        handler.handleRequest(input, output, context);
+
+        ArgumentCaptor<PutObjectRequest> putObjectRequestCaptor = ArgumentCaptor.forClass(PutObjectRequest.class);
+        ArgumentCaptor<RequestBody> requestBodyCaptor = ArgumentCaptor.forClass(RequestBody.class);
+
+        verify(s3Client).putObject(putObjectRequestCaptor.capture(), requestBodyCaptor.capture());
+
+        PutObjectRequest capturedPutObjectRequest = putObjectRequestCaptor.getValue();
+        RequestBody capturedRequestBody = requestBodyCaptor.getValue();
+        var contentStreamProvider = capturedRequestBody.contentStreamProvider();
+        String fileContent = getFileContent(contentStreamProvider);
+
+        assertEquals(TEST_BUCKET, capturedPutObjectRequest.bucket());
+        assertEquals(UNITS_JSON_FILENAME, capturedPutObjectRequest.key());
+        assertEquals(1370603, fileContent.length());
+        assertTrue(fileContent.contains("https://api.cristin.no/v2/units/184.13.53.0"));
+    }
+
+    private static String getFileContent(ContentStreamProvider contentStreamProvider) {
+        return new Scanner(contentStreamProvider.newStream(), StandardCharsets.UTF_8).useDelimiter(DELIMITER).next();
+    }
+}
