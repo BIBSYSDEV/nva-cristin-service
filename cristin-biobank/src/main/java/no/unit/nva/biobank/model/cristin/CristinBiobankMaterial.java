1ca3d67dcc73a351ae85c61dd0fcfc3c709193b0
torarnet <6398417+torarnet@users.noreply.github.com>
2024-07-04 13:46:09 +0200

    Np 47035 refactor classes to records other modules (#395)
    * np-47035: use records in biobank module
    
    * np-47035: refactor biobank adapter

--- cristin-biobank/src/main/java/no/unit/nva/biobank/model/cristin/CristinBiobankMaterial.java
+++ cristin-biobank/src/main/java/no/unit/nva/biobank/model/cristin/CristinBiobankMaterial.java
@@ -1,52 +1,23 @@
 package no.unit.nva.biobank.model.cristin;
 
+import static no.unit.nva.cristin.common.Utils.nonEmptyOrDefault;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import java.beans.ConstructorProperties;
 import java.util.Map;
-import java.util.Objects;
 import no.unit.nva.commons.json.JsonSerializable;
 import nva.commons.core.JacocoGenerated;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class CristinBiobankMaterial implements JsonSerializable {
-
-    private final String materialCode;
-    private final Map<String, String> materialName;
-
-    @ConstructorProperties({"materialCode", "materialName"})
-    public CristinBiobankMaterial(String materialCode, Map<String, String> materialName) {
-        this.materialCode = materialCode;
-        this.materialName = materialName;
-    }
-
-    public String getMaterialCode() {
-        return materialCode;
-    }
-
-    public Map<String, String> getMaterialName() {
-        return materialName;
-    }
+public record CristinBiobankMaterial(@JsonProperty("material_code") String materialCode,
+                                     @JsonProperty("material_name") Map<String, String> materialName)
+    implements JsonSerializable {
 
     @Override
-    @JacocoGenerated
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof CristinBiobankMaterial that)) {
-            return false;
-        }
-        return Objects.equals(getMaterialCode(), that.getMaterialCode())
-               && Objects.equals(getMaterialName(), that.getMaterialName());
-    }
-
-    @Override
-    @JacocoGenerated
-    public int hashCode() {
-        return Objects.hash(getMaterialCode(), getMaterialName());
+    public Map<String, String> materialName() {
+        return nonEmptyOrDefault(materialName);
     }
 
     @Override
@@ -54,4 +25,5 @@ public class CristinBiobankMaterial implements JsonSerializable {
     public String toString() {
         return toJsonString();
     }
+
 }
