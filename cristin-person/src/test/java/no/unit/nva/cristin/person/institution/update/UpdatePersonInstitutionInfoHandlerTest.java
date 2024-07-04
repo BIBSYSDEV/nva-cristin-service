e85ddf9f081009a92f3aa28ab3214d972598056a
torarnet <6398417+torarnet@users.noreply.github.com>
2024-07-04 10:14:12 +0200

    np-47035: use multiline string for json

--- cristin-person/src/test/java/no/unit/nva/cristin/person/institution/update/UpdatePersonInstitutionInfoHandlerTest.java
+++ cristin-person/src/test/java/no/unit/nva/cristin/person/institution/update/UpdatePersonInstitutionInfoHandlerTest.java
@@ -121,12 +121,26 @@ public class UpdatePersonInstitutionInfoHandlerTest {
     }
 
     private PersonInstInfoPatch defaultBody() throws JsonProcessingException {
-        var body = "{\"email\":\"test@example.com\", \"phone\":\"99112233\"}";
+        var body =
+            """
+              {
+                "email": "test@example.com",
+                "phone":"99112233"
+              }
+            """;
+
         return OBJECT_MAPPER.readValue(body, PersonInstInfoPatch.class);
     }
 
     private PersonInstInfoPatch bodyWithUnsupportedFields() throws JsonProcessingException {
-        var body = "{\"hello\":\"world\", \"lorem\":\"ipsum\"}";
+        var body =
+            """
+              {
+                "hello": "world",
+                "lorem": "ipsum"
+              }
+            """;
+
         return OBJECT_MAPPER.readValue(body, PersonInstInfoPatch.class);
     }
 }
