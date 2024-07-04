56f4b10bd80641f6d75cf533ac2617eef38facfe
torarnet <6398417+torarnet@users.noreply.github.com>
2024-07-04 12:26:58 +0200

    np-47035: use records in person module when it is an easy conversion (#394)

--- cristin-person/src/main/java/no/unit/nva/cristin/person/model/nva/Role.java
+++ cristin-person/src/main/java/no/unit/nva/cristin/person/model/nva/Role.java
@@ -1,52 +1,19 @@
 package no.unit.nva.cristin.person.model.nva;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import static no.unit.nva.cristin.common.Utils.nonEmptyOrDefault;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
 import nva.commons.core.JacocoGenerated;
 
 @SuppressWarnings("PMD.ShortClassName")
 @JacocoGenerated
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public class Role {
+public record Role(@JsonProperty("labels") Map<String, String> labels) {
 
-    private final Map<String, String> labels;
-
-    /**
-     * Creates a Role for serialization to client.
-     *
-     * @param labels Labels in different languages describing this role.
-     */
-    @JsonCreator
-    public Role(@JsonProperty("labels") Map<String, String> labels) {
-        this.labels = labels;
-    }
-
-
-    public Map<String, String> getLabels() {
-        return Objects.nonNull(labels) ? labels : Collections.emptyMap();
-    }
-
-    @JacocoGenerated
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Role)) {
-            return false;
-        }
-        Role that = (Role) o;
-        return Objects.equals(getLabels(), that.getLabels());
-    }
-
-    @JacocoGenerated
-    @Override
-    public int hashCode() {
-        return Objects.hash(getLabels());
+    public Map<String, String> labels() {
+        return nonEmptyOrDefault(labels);
     }
 
 }
